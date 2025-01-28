a3f2513a5ed5faa5afc9498ac20069c25dc58f07
Shaun Newman <shaun.newman.p@gmail.com>
2025-01-28 15:29:11 -0500

    added try/catch to the async funcction

--- index.js
+++ index.js
@@ -5,14 +5,20 @@ const state = {
 
 // create async function for pary API
 const getParty = async() => {
+  try {
   // fetch the pary API
   const response = await fetch(`https://fsa-crud-2aa9294fe819.herokuapp.com/api/2501-ftb-et-web-ft/events`);
   // convert to json
   const jsonObject = await response.json();
+  // target the data in the json object
   const allParties = jsonObject.data;
+  // add the objects to the state array
   state.partyList = allParties;
-  console.log(state.partyList);
+  // invote the render function
   renderParties();
+  } catch (error) {
+    alert(error);
+  }
 }
 
 // function to render to page
@@ -26,7 +32,8 @@ const renderParties = () => {
     // create a new li
     const li = document.createElement('li');
     // add inner text to li
-    li.innerText = `PARTY NAME: ${singleParty.name} DATE: ${singleParty.date} DESCRIPTION:${singleParty.description} LOCATION: ${singleParty.location}`
+    li.innerText = `PARTY NAME: ${singleParty.name} .\n DATE: ${singleParty.date} .\n DESCRIPTION:${singleParty.description} .\n LOCATION: ${singleParty.location}`
+    // add the new li to the UL node
     partyUL.append(li);
   })
   
